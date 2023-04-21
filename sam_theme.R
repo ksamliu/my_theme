a17e0d8673d5e26728ec2a731e8a4c466655ba38
KSL <63486018+ksamliu@users.noreply.github.com>
2023-04-21 16:30:00 -0400

    Create sam_theme.R

---
+++ sam_theme.R
@@ -0,0 +1,11 @@
+theme(
+  panel.background = element_rect(fill = "white"),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.line.x = element_line(size = .3, colour = "black"),
+  axis.line.y = element_blank(),
+  axis.text = element_text(colour = "black"),
+  axis.title = element_text(colour = "black"),
+  axis.ticks.x = element_line(colour = "black"),
+  axis.ticks.length = unit(0.1, "cm")
+)
